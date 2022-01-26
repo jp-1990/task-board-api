4013e9f6a0938ba55055f9ed29f59123457a943d
James Plummer <plum_1990@hotmail.co.uk>
2022-01-26 23:34:27 +0000

    [update] added list_id to update task index query to allow moving tasks between lists

--- src/queries/update-task-index.ts
+++ src/queries/update-task-index.ts
@@ -1,5 +1,5 @@
-export const updateTaskIndexQuery = ({ id, index }) => {
+export const updateTaskIndexQuery = ({ id, index, list_id }) => {
   return `
-    UPDATE tasks SET index= '${index}' WHERE task_id= '${id}' RETURNING task_id, list_id, index, name, description, deadline, completed, overdue, type
+    UPDATE tasks SET index= '${index}', list_id= '${list_id}' WHERE task_id= '${id}' RETURNING task_id, list_id, index, name, description, deadline, completed, overdue, type
     `;
 };
